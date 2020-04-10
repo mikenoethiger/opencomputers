6e5d0891cb7a236da6a9fd99522be83723cb63b0
Mike Nöthiger <noethiger.mike@gmail.com>
2020-04-10 16:15:31 +0200

    add program to download from github, add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Links
+=====
+
+* [OpenComputers Wiki](https://ocdoc.cil.li/)
+* [Lua APIs](https://ocdoc.cil.li/api)
+* [Shell API](https://ocdoc.cil.li/api:shell)
---
+++ github.lua
@@ -0,0 +1,23 @@
+local shell = require("shell")
+
+local args, options = shell.parse(...)
+
+if #args < 1 then
+    io.write("Usage: github <repo_file> [<filename>]")
+    io.write("Downloads a file from the github.com/mikenoethiger/opencomputers repository.")
+    return
+end
+
+repo_url = "https://raw.githubusercontent.com/mikenoethiger/opencomputers/master/"
+
+function prog_url(prog_name)
+    return repo_url .. prog_name
+end
+
+local filename = ""
+if #args > 1 then
+    filename = " " .. args[2]
+end
+
+result = shell.execute("wget -f " .. prog_url() .. filename)
+print(result)
