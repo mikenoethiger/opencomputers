a5254daf80cb467a70c58ceeaceabb3ab914f092
Mike Nöthiger <noethiger.mike@gmail.com>
2020-04-10 16:52:31 +0200

    update github program

--- github.lua
+++ github.lua
@@ -4,6 +4,8 @@ local args, options = shell.parse(...)
 
 if #args < 1 then
     io.write("Usage: github <program> [<filename>]\n")
+    io.write("    program: the program name relative to the repository root (with or without .lua ending)\n")
+    io.write("    filename: filename of the download (if not specified the program name is the default)\n")
     io.write("Downloads a program from the github.com/mikenoethiger/opencomputers repository.\n")
     return
 end
@@ -24,4 +26,4 @@ if #args > 1 then
     filename = " " .. args[2]
 end
 
-result = shell.execute("wget -f " .. prog_url(args[1]) .. filename)
+result = shell.execute("wget -f " .. url .. filename)
